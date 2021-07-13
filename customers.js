aa9d041570680c0cd95079077cc81b3568c3716c
emmilyyy111 <86429560+emmilyyy111@users.noreply.github.com>
2021-07-12 23:00:31 -0400

    Add files via upload

--- customers.js
+++ customers.js
@@ -2,23 +2,74 @@ const admin = require('firebase-admin')
 const credentials = require('../credentials.json')
 
 function connectDB() {
-    if(!admin.apps.length){
+  if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(credentials)
-        })
-    }
-    return admin.firestore()
+      credential: admin.credential.cert(credentials),
+    })
+  }
+  return admin.firestore()
 }
 
 exports.getCustomers = (request, response) => {
-    const db = connectDB()
-    db.collection('customers').get()
-        .then(customerCollection => {
-            const allCustomers = customerCollection.docs.map(doc => doc.data())
-            response.send(allCustomers)
-        })
-        .catch(err => {
-            console.error(err)
-            response.status(500).send(err)
-        })
+  const db = connectDB()
+  db.collection('customers')
+    .get()
+    .then(customerCollection => {
+      const allCustomers = customerCollection.docs.map(doc => doc.data())
+      response.send(allCustomers)
+    })
+    .catch(err => {
+      console.error(err)
+      response.status(500).send(err)
+    })
+}
+exports.getCustomerById = (request, response) => {
+  if (!request.params.id) {
+    response.status(400).send('No customer specified!')
+    return
+  }
+  const db = connectDB()
+  db.collection('customers')
+    .doc(request.params.id)
+    .get()
+    .then(doc => {
+      let customer = doc.data()
+      customer.id = doc.id
+      response.send(customer)
+    })
+    .catch(err => {
+      console.error(err)
+      response.status(500).send(err)
+    })
+}
+
+exports.getCustomerByQuery = (req, res) => {
+  // get query from req.query
+  const { fname } = req.query
+  // connect to firestore
+  const db = connectDB()
+  // search customers collection based on query
+  db.collection('customers')
+    .where('firstName', '==', fname)
+    .get()
+    .then(customerCollection => {
+      const matches = customerCollection.docs.map(doc => {
+        let customer = doc.data()
+        customer.id = doc.id
+        return customer
+      })
+      res.send(matches)
+    })
+    .catch(err => res.status(500).send(err))
+}
+
+// create post to createCustomer
+exports.createCustomer = (req, res) => {
+  const db = connectDB() // connect to db
+
+  
+  db.collection('customers')
+    .add(bodyParser.json(req.body))
+    .then(docRef => res.send(docRef.id))
+    .catch(err => res.status(500).send('Customer could not be created'))
 }
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-const { getCustomers } = require('./src/customers')
+const { getCustomers, getCustomerById, getCustomerByQuery, createCustomer } 
+    = require('./src/customers') //how to get a function from another directory; an import
 
-const app = express()
-app.use(cors())
+const app = express() // initializing our app with express
+app.use(cors()) //making app use cors
 
-app.get('/customers', getCustomers)
-// app.post('/customers', createCustomer)
+app.get('/customers/search', getCustomerByQuery)
+app.get('/customers/:id', getCustomerById)
+app.get('/customers', getCustomers) // telling app to run getCustomers app when you go this route .get gets data
+app.post('/customers', createCustomer)
 
 app.listen(3000, () => {
     console.log('Listening to http://localhost:3000')
